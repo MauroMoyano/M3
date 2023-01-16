86a55d391fb0c983938881726ce613014b05cef2
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-01-16 03:59:37 -0300

    salvando datos para que continue jehovander

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -4,7 +4,22 @@ const {stringify} = require("nodemon/lib/utils");
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-let publications = [];
+let publications = [{
+    id: "0",
+    author: "Author Test 1",
+    title: "Title Test 3",
+    contents: "Content Testing 3",
+},{
+    id: "1",
+    author: "Author Test 2",
+    title: "Title Test 3",
+    contents: "Content Testing 3",
+},{
+    id: "2",
+    author: "Author est 3",
+    title: "Title est 3",
+    contents: "Content esting 3",
+}];
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -38,18 +53,20 @@ server.post("/posts", (req, res) => {
 })
 
 server.get("/posts", (req, res)=>{
-
+    const {term} = req.query;
+    console.log(term)
+    if(!!term){
+            const termsWords = publications.filter((elemento)=> elemento.title.includes(term) || elemento.contents.includes(term));
+            console.log("termsWord arreglo filtrado :" + termsWords)
+            res.status(200).json(termsWords)
+        } else {
+            console.log("publicaciones arreglo :" + publications)
+            res.status(206).json(publications)
+        }
 })
 
 
-/*if(req.query.term){
-        const termsWords = publications.filter((elemento)=>{
-            elemento.title.includes("term")
-        })
-        res.end(JSON.stringify(termsWords))
-    } else {
-        res.end(JSON,stringify(publications))
-    }*/
+
 
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = {publications, server};
